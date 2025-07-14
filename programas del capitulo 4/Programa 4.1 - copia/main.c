e1f0a67ec445fb636da6fccb0f8d6a2622902836
puraacosta <puraacosta15@gmail.com>
2025-07-13 23:14:00 -0400

    Add files via upload

---
+++ programas del capitulo 4/Programa 4.1 - copia/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* cubo  de un numero
+realizado por pura acosta
+*/
+
+int cubo (int n)
+
+{
+    return n * n * n;
+
+}
+
+int main(void)
+
+{
+   for (int i = i; i <=10; i++)
+    printf("El cubo de %d es: %d\n" , i, cubo(i));
+
+   return 0;
+
+}
